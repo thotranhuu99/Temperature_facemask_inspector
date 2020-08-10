53bd26724ff1169ca5c581f589c53d5c9a608847
thotranhuuu99 <thotranhuu99@gmail.com>
2020-08-10 09:59:12 +0700

    Second commmit

--- Test_folder/client.py
+++ Test_folder/client.py
@@ -6,4 +6,5 @@ while True:
     b = bytearray(img)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(b, ("192.168.100.6", 5002))
+    print("Frame sent")
     time.sleep(0.1)
--- Test_folder/server.py
+++ Test_folder/server.py
@@ -9,13 +9,13 @@ sock.bind(server_address)
 i = 0
 while True:
     data, client_address = sock.recvfrom(10240)
-    #print(data)
-    #print(client_address)
+    # print(data)
+    # print(client_address)
     serialized_bytes = np.frombuffer(data, dtype=np.uint16)
     image_received = np.reshape(serialized_bytes, newshape=(60, 80))
     cv2.imwrite(os.path.join('/mnt/ramdisk', 'temp_image.png'), image_received)
-    #img = cv2.imread('Test.png', -1)
-    #cv2.imshow('Window 1', img)
+    # img = cv2.imread('Test.png', -1)
+    # cv2.imshow('Window 1', img)
     k = cv2.waitKey(100) & 0xFF
     if k == 27:
         break
