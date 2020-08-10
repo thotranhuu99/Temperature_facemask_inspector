a8b442afd9270f441130f65e64334854fcd8d89f
thotranhuuu99 <thotranhuu99@gmail.com>
2020-08-10 12:09:34 +0700

    Testing

---
+++ Test_folder/Convert_to_normal.sh
@@ -0,0 +1,1 @@
+ convert /mnt/ramdisk/temp_image.png -normalize -contrast -sharpen 5 y.png; fbi -a y.png
--- Test_folder/server.py
+++ Test_folder/server.py
@@ -1,12 +1,13 @@
+import subprocess
 import cv2
 import socket
 import numpy as np
 import os
 
+Bash_convert_script_location = os.path.join(os.getcwd(), "Convert_to_normal.sh")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ("192.168.100.6", 5002)
 sock.bind(server_address)
-i = 0
 while True:
     data, client_address = sock.recvfrom(10240)
     # print(data)
@@ -14,7 +15,9 @@ while True:
     serialized_bytes = np.frombuffer(data, dtype=np.uint16)
     image_received = np.reshape(serialized_bytes, newshape=(60, 80))
     cv2.imwrite(os.path.join('/mnt/ramdisk', 'temp_image.png'), image_received)
-    cv2.imshow('Window 1', image_received)
+    rc = subprocess.call(Bash_convert_script_location, shell=True)
+    img_converted = cv2.imread("y.png")
+    cv2.imshow('Window 1', img_converted)
     k = cv2.waitKey(100) & 0xFF
     if k == 27:
         break
