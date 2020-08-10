0842261e21b82cf547ef905c224d39d4adc61cec
thotranhuuu99 <thotranhuu99@gmail.com>
2020-08-10 11:19:41 +0700

    Testing

--- Test_folder/Raspberry_bash_script_test.py
+++ Test_folder/Raspberry_bash_script_test.py
@@ -1,4 +1,7 @@
 import subprocess
-print("start")
-rc = subprocess.call("/home/tho/Lepton_Project/Test_folder/Test_bash.sh", shell=True)
-print("stop")
+import os
+
+Bash_script_location = os.path.join(os.getcwd(), "flirpi", "single_read.sh")
+while True:
+    # rc = subprocess.call("/home/tho/Lepton_Project/Test_folder/Test_bash.sh", shell=True)
+    rc = subprocess.call(Bash_script_location, shell=True)
--- Test_folder/Read_img.py
+++ Test_folder/Read_img.py
@@ -1,5 +1,8 @@
 import cv2
-img = cv2.imread("y.png", cv2.IMREAD_GRAYSCALE)
+import os
+Working_dir = os.getcwd()
+Image_location = os.path.join(Working_dir, "flirpi", "y.png")
+img = cv2.imread(Image_location, cv2.IMREAD_GRAYSCALE)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 
