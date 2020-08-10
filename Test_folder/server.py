6d3d62f51075f4d19d9701df0202f5f7a67a61eb
Nguyen Thanh Khai Tam <khaitam.vt@gmail.com>
2020-08-10 19:15:29 +0700

    Change address

--- Test_folder/server.py
+++ Test_folder/server.py
@@ -5,12 +5,12 @@ import os
 
 Bash_convert_script_location = os.path.join(os.getcwd(), "Convert_to_normal.sh")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ("192.168.100.6", 5002)
+server_address = ("192.168.100.255", 5002)
 sock.bind(server_address)
 while True:
     data, client_address = sock.recvfrom(10240)
-    # print(data)
-    # print(client_address)
+    print(data)
+    print(client_address)
     serialized_bytes = np.frombuffer(data, dtype=np.uint16)
     raw_image_received = np.reshape(serialized_bytes, newshape=(60, 80))
     print(" Max_val = {}\n Raw temp = {}".format(np.amax(raw_image_received), np.amax(raw_image_received)/100-273.3))
