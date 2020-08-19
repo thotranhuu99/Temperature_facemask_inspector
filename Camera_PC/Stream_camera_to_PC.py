462812c873175985c0c8bb23d1ee7501cef2136d
thotranhuuu99 <thotranhuu99@gmail.com>
2020-08-19 22:40:34 +0700

    Camera stream working

--- Camera_PC/Stream_camera_to_PC.py
+++ Camera_PC/Stream_camera_to_PC.py
@@ -1,17 +1,29 @@
 import io
 import time
 import picamera
+#from PIL import Image
+
+def outputs():
+    stream = io.BytesIO()
+    for i in range(40):
+        # This returns the stream for the camera to capture to
+        yield stream
+        # Once the capture is complete, the loop continues here
+        # (read up on generator functions in Python to understand
+        # the yield statement). Here you could do some processing
+        # on the image...
+        #stream.seek(0)
+        #img = Image.open(stream)
+        # Finally, reset the stream for the next capture
+        stream.seek(0)
+        stream.truncate()
+
 
 with picamera.PiCamera() as camera:
-    # Set the camera's resolution to VGA @40fps and give it a couple
-    # of seconds to measure exposure etc.
     camera.resolution = (640, 480)
     camera.framerate = 80
     time.sleep(2)
-    # Set up 40 in-memory streams
-    outputs = [io.BytesIO() for i in range(40)]
     start = time.time()
-    camera.capture_sequence(outputs, 'jpeg', use_video_port=True)
+    camera.capture_sequence(outputs(), 'jpeg', use_video_port=True)
     finish = time.time()
-    # How fast were we?
     print('Captured 40 images at %.2ffps' % (40 / (finish - start)))
