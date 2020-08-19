2f69347c0f81e2d7082573ffe1626ce5e4042b6b
thotranhuuu99 <thotranhuu99@gmail.com>
2020-08-19 22:37:02 +0700

    Camera stream working

--- Camera_PC/Stream_camera_to_PC.py
+++ Camera_PC/Stream_camera_to_PC.py
@@ -1,8 +1,17 @@
-from picamera import PiCamera
-from time import sleep
+import io
+import time
+import picamera
 
-camera = PiCamera()
-
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
+with picamera.PiCamera() as camera:
+    # Set the camera's resolution to VGA @40fps and give it a couple
+    # of seconds to measure exposure etc.
+    camera.resolution = (640, 480)
+    camera.framerate = 80
+    time.sleep(2)
+    # Set up 40 in-memory streams
+    outputs = [io.BytesIO() for i in range(40)]
+    start = time.time()
+    camera.capture_sequence(outputs, 'jpeg', use_video_port=True)
+    finish = time.time()
+    # How fast were we?
+    print('Captured 40 images at %.2ffps' % (40 / (finish - start)))
