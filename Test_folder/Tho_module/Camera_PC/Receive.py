532e505b56ced813e0426a95cde87ae7c9d1ad79
thotranhuuu99 <thotranhuu99@gmail.com>
2020-08-28 18:58:40 +0700

    Camera stream working

--- Test_folder/Tho_module/Camera_PC/Receive.py
+++ Test_folder/Tho_module/Camera_PC/Receive.py
@@ -2,7 +2,8 @@ import io
 import socket
 import struct
 from PIL import Image
-
+import time
+import cv2
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
 # all interfaces)
 server_socket = socket.socket()
@@ -13,6 +14,7 @@ server_socket.listen(0)
 connection = server_socket.accept()[0].makefile('rb')
 try:
     while True:
+        start_time = time.time()
         # Read the length of the image as a 32-bit unsigned int. If the
         # length is zero, quit the loop
         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
@@ -26,9 +28,11 @@ try:
         # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
-        print('Image is %dx%d' % image.size)
-        image.verify()
-        print('Image is verified')
+        stop_time = time.time()
+        #print('Image is %dx%d' % image.size)
+        #image.verify()
+        #print('Image is verified')
+        print("%.2f" % (stop_time-start_time))
 finally:
     connection.close()
     server_socket.close()
