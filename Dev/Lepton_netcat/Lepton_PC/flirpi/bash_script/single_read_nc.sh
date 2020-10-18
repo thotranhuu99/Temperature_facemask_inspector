3d70a0f2d96e34dc71766ffdbb186fe8a3154ec8
thotranhuu99 <thotranhuu99@gmail.com>
2020-10-18 12:20:16 +0700

    Lepton netcat working

--- Dev/Lepton_netcat/Lepton_PC/flirpi/bash_script/single_read_nc.sh
+++ Dev/Lepton_netcat/Lepton_PC/flirpi/bash_script/single_read_nc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)"
 Single_read_loc="${DIR}/leptgraypng"
-SCRIPT="${Single_read_loc} > /mnt/ramdisk/temp.png;"
+SCRIPT="${Single_read_loc} >/mnt/ramdisk/temp.png;"
 #printf "${SCRIPT_LOC}"
-printf "SCRIPT"
-${SCRIPT}
+eval ${SCRIPT}
